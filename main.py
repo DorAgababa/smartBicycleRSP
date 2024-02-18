f5efcd6f9677ca83c39441d212d1e40ffd83a545
DorAgababa <dor16agababa@gmail.com>
2024-02-18 21:47:38 +0200

    FirstTry[David Oved]: stom
    Signed-off-by: DorAgababa <dor16agababa@gmail.com>

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+rounds = 0 
+roundsSec = 0 
+radius = 0.25
+
+
+def main():
+    print("dor gay :D")
+    startTime=time.time()
+    time.sleep(3)
+    print(f"time {time.time()-startTime}")
+main()
