885fd70d2533c7eeff16433a519674af6439d95d
DorAgababa <dor16agababa@gmail.com>
2024-02-25 22:26:55 +0200

    New_Features: Added interrupt handler for magnet and reset for rounds_sec each second
    Signed-off-by: DorAgababa <dor16agababa@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,64 @@
 import time
-rounds = 0 
-roundsSec = 0 
+import signal
+import threading
+from gpiozero import Button
+
+
+# GPIO pin connected to the magnetic sensor that count number of rounds
+MAGNETIC_SENSOR_PIN = 2
+rounds = 0 #describe the total rounds done till now
+roundsSec = 0 #use for describe the rounds for the last second
 radius = 0.25
+# Create a lock for synchronization the rounds_sec_lock variable
+rounds_sec_lock = threading.Lock()
+
+#calling itself each second
+def update_rounds_sec():
+    global roundsSec
+    #update the gui with the current value
+    print("rounds for last sec:" ,roundsSec)
+    with rounds_sec_lock:
+        roundsSec = 0
+    timer = threading.Timer(1, update_rounds_sec)
+    timer.daemon = True # make the thread terminated on the end of the main
+    timer.start()
+        
+    
+def intterupt_handler_magnetic_sensor():
+    global rounds
+    global roundsSec
+    rounds += 1
+    with rounds_sec_lock:
+        roundsSec += 1
+    print("rounds:" ,rounds)
+    print("rounds for last sec:" ,roundsSec)
+
+# Function to nicely exit the program
+def exit_program_gracefully():
+    print("Exiting program...")
+    magnetic_sensor.close()
+    exit(0)
+
+def exit_program(signal, frame):
+   exit_program_gracefully()
+
+def setup_magnetic_sensor():
+    magnetic_sensor = Button(MAGNETIC_SENSOR_PIN)
+    magnetic_sensor.when_pressed = intterupt_handler_magnetic_sensor
+    
+    # Set up signal handler for program exit
+    signal.signal(signal.SIGINT, exit_program)
+    
+    return magnetic_sensor
+
+# Set up magnetic sensor
+magnetic_sensor = setup_magnetic_sensor()
 
 def main():
     startTime=time.time()
-    time.sleep(3)
-    print(f"time {time.time()-startTime}")
-main()
+    update_rounds_sec()
+    time.sleep(100)
+    exit_program_gracefully()
+# Entry point
+if __name__ == "__main__":
+    main()
